f2ed9d49c8b2f16e8afed537a6d6b429ee981989
YPermitin <ypermitin@yandex.ru>
2017-01-19 21:32:51 +0500

    #3 Добавил скрипт для работы с библиотекой "ReleasesGetter", получающей информацию с сайта 'users.v8.1c.ru'

---
+++ oce-releases/oce-releases.psm1
@@ -0,0 +1,26 @@
+﻿function Get-ActualReleasesInfo
+<#
+.Synopsis
+   Получение списка актуальных релизов с сайта users.v8.1c.ru
+.DESCRIPTION
+   Получение списка актуальных релизов с сайта users.v8.1c.ru
+.NOTES  
+    Name: oceHelper
+    Author: ypermitin@yandex.ru
+.LINK  
+    https://github.com/YPermitin/PowerShell-For-1C-Developer
+.EXAMPLE
+   Get-ActualReleases
+.OUTPUTS
+   Объект класса 'ReleasesGetter' для работы с данными сайта 'users.v8.1c.ru/actual.jsp'
+#>
+{
+    Param(    )
+    
+    $oceReleasesLib = (Get-Module oce-releases).Path.TrimEnd('oce-releases.psm1') + "libs\DevelPlatform.OCE.GetterPlatformInfo.dll"
+    Add-Type -Path $oceReleasesLib
+
+    $releasesGetter = new-object DevelPlatform.OCE.GetterReleasesInfo.ReleasesGetter
+
+    $releasesGetter
+}
